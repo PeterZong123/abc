97880852ba431f88910f268d1c4b592801609d08
liangzhifeng <zhifeng.liang@honeywell.com>
2017-10-17 14:38:31 +0800

    init

--- src/app/routes/routes.module.ts
+++ src/app/routes/routes.module.ts
@@ -1,20 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { SharedModule } from '@shared/shared.module';
-import { PagesModule } from './pages/pages.module';
+//import { PagesModule } from './pages/pages.module';
 
 import { routes } from './routes';
-import { DashboardV1Component } from './dashboard/v1/v1.component';
+//import { DashboardV1Component } from './dashboard/v1/v1.component';
 
 @NgModule({
     imports: [
         SharedModule,
-        RouterModule.forRoot(routes, { useHash: true }),
-        PagesModule
+        RouterModule.forRoot(routes, { useHash: true })
+        //PagesModule
     ],
-    declarations: [
-        DashboardV1Component
-    ],
+
     exports: [
         RouterModule
     ]
