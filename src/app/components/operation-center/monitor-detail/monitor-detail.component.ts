b110d37d23ae7bf1099e9b14010bd89b7dc6c4a8
bilibobo <1905516073@qq.com>
2017-11-14 15:48:55 +0800

    CPU echarts图

--- src/app/components/operation-center/monitor-detail/monitor-detail.component.ts
+++ src/app/components/operation-center/monitor-detail/monitor-detail.component.ts
@@ -9,13 +9,72 @@ import { ActivatedRoute } from '@angular/router';
 export class MonitorDetailComponent implements OnInit {
   
   private appName: string;
-
+  private cpuOption: any;
   constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
     this.activatedRoute.params.subscribe( params => {
       this.appName = params['name'];
     })
+    this.cpuOption = {
+      title: {
+        text: this.appName + ' CPU Usage'
+      },
+      tooltip : {
+        trigger: 'axis'
+      },
+      legend: {
+        data:['app1','app2','app3','app4','app5']
+      },
+      grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true,
+      },
+      xAxis : [
+        {
+          type : 'category',
+          boundaryGap : false,
+          data : ['周一','周二','周三','周四','周五','周六','周日']
+        }
+      ],
+      yAxis : [
+        {
+          type : 'value',
+          splitArea: {
+            show: true
+          }
+        }
+      ],
+      series : [
+        {
+          name:'app1',
+          type:'line',
+          data:[120, 132, 101, 134, 90, 230, 210]
+        },
+        {
+          name:'app2',
+          type:'line',
+          data:[220, 182, 191, 234, 290, 330, 310]
+        },
+        {
+          name:'app3',
+          type:'line',
+          data:[150, 232, 201, 154, 190, 330, 410]
+        },
+        {
+          name:'app4',
+          type:'line',
+          data:[320, 332, 301, 334, 390, 330, 320]
+        },
+        {
+          name:'app5',
+          type:'line',
+          data:[820, 932, 901, 934, 1290, 1330, 1320]
+        }
+      ]
+    }
   }
-
+  
 }
--- src/app/components/operation-center/operation-center.module.ts
+++ src/app/components/operation-center/operation-center.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from 'app/shared/shared.module';
+import { AngularEchartsModule } from 'ngx-echarts';
 import { OperationRoutes } from './operation-center.router';
 import { HistoryComponent } from './history/history.component';
 import { LogComponent } from './log/log.component';
@@ -15,6 +16,7 @@ import { MonitorDetailComponent } from './monitor-detail/monitor-detail.componen
     ],
     imports: [
         OperationRoutes,
+        AngularEchartsModule,
         SharedModule,
     ],
     exports: [],
