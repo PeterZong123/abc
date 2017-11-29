076a85a3b808f2a52a786cdfafc2d17dfa7eef1d
bilibobo <1905516073@qq.com>
2017-11-29 15:15:08 +0800

    添加token过期

--- src/app/shared/routerControl/can-activate-guard.ts
+++ src/app/shared/routerControl/can-activate-guard.ts
@@ -1,19 +1,21 @@
 import { Injectable } from '@angular/core';
 import { CanActivateChild, Router } from '@angular/router';
 import { LoginService } from '../../pages/service/login.service';
-
+import { environment } from './../../../environments/environment';
 @Injectable()
 export class CanActivateGuard implements  CanActivateChild{
   constructor(private router: Router, private loginService: LoginService){}
-
+  
   canActivateChild(){
-    return true;
-    // if (this.loginService.loggedIn()){
-    //   return true;
-    // }else{
-    //   this.router.navigateByUrl('/login');
-    //   console.log('token验证失败');
-    //   return false;
-    // }
+    if(!environment.production){
+      return true;
+    }
+    if (this.loginService.loggedIn()){
+      return true;
+    }else{
+      this.router.navigateByUrl('/login');
+      console.log('token验证失败');
+      return false;
+    }
   }
 }
